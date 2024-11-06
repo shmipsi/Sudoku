0f95987baf96a0b5f8b15729a266531693dd1195
shmipsi <shmipsi@proton.me>
2024-11-07 00:04:02 +0200

    completed part for checking correctiveness started next parts

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,8 @@
 import colored
 
+# 2
 b2 = [
-    [3, 9, 1, 2, 8, 6, 5, 7, 4],
+    [3, 9, 1, "-", 8, 6, 5, 7, 4],
     [4, 8, 7, 3, 5, 9, 1, 2, 6],
     [6, 5, 2, 7, 1, 4, 8, 3, 9],
     [8, 7, 5, 4, 3, 1, 6, 9, 2],
@@ -38,31 +39,89 @@ def print_array(array):
         if line_number==2 or line_number==5 or line_number==8: print(*line_one)
         line_number += 1
 
+# dup checks
+def dup_check_line(array,i,j):
+    dup = array[i][j]
+    for k in range(9):
+        if j!=k and dup==array[i][k]: return 0
+    return 1
+
 # check if sudoku solved right
 # 1-right 0-wrong
-def sudoku_check(array):
-    # check horizontal sum
+def sudoku_check_correctness(array):
+    # check horizontal sum and duplicates
     for i in range(9):
         sum_hor = 0
         for j in range(9):
             sum_hor += array[i][j]
-        if sum_hor != 45: return 0
-    # check vertical sum
-    for i in range(9):
+            dup_check_line(array,i,j)
+        if sum_hor!=45: return 0
+    # check vertical sum and duplicates
+    for j in range(9):
         sum_vert = 0
-        for j in range(9):
-            sum_vert += array[j][i]
+        for i in range(9):
+            sum_vert += array[i][j]
+            dup_check_line(array,i,j)
         if sum_vert != 45: return 0
-    # check cell sum
+    # check cell sum and duplicates
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
             sum_cell = 0
             for k in range(3):
                 for l in range(3):
                     sum_cell += array[i + k][j + l]
+                    dup = array[i+k][j+l]
+                    for n in range(3):
+                        for m in range(3):
+                            if k!=n and l!=m and dup==array[i+n][j+m]: return 0
             if sum_cell != 45: return 0
     return 1
 
-print_array(b2)
-if sudoku_check(b2): print("That's correct!")
-else: print("Nah that's bullshit")
+# solve the sudoku
+def sudoku_solve(array):
+    empty_cells=[[],[]]
+    for i in range(9):
+        for j in range(9):
+            if array[i][j]=="-":
+                empty_cells[0].append(i)
+                empty_cells[1].append(i)
+    range_l = len(empty_cells[0])
+    digit = 1
+    # there must be something else so everything tryes itself again if not correct
+    for k in range(range_l):
+        i = empty_cells[0][k]
+        j = empty_cells[1][k]
+        array[i][j] = digit
+        print(digit)
+        if dup_check_line(array,i,j) == 0: digit += 1
+        # add here vert and cell check
+        print_array(array)
+        if digit > 9:
+            digit = 0
+            continue
+        if i==9 and j==9 and digit>9: return "Nah that's bullshit"
+    return array
+
+def switch(value):
+    if value==1:
+        print_array(b2)
+    elif value==2:
+        print_array(b2)
+    elif value==3:
+        print_array(b2)
+        b2 = sudoku_solve(b2)
+    elif value==4:
+        print_array(b2)
+        if sudoku_check_correctness(b2): print("That's correct!")
+        else: print("Nah that's bullshit")
+    else: print("error")
+
+
+# main code
+print("-------------------------Welcome to Sudoku!-------------------------")
+print("Now you can choose what you want to do:")
+print("1 - check if sudoku is valid")
+print("2 - give you a sudoku to solve")
+print("3 - solve the sudoku")
+print("4 - tell you if sudoku solved correctly")
+switch(int(input()))
